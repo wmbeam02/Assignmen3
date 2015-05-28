aa60dabaa85d1d171ff46b4311e7d90faf117dfa
Matt Beam <wmbeam02@users.noreply.github.com>
2015-05-28 11:29:30 -0500

    Update best1.R

--- best1.R
+++ best1.R
@@ -1,51 +1,38 @@
-# Now I'm trying to change your code
-
-setwd("~/Desktop/Online Coursera/Coursera-R-Programming/week4/")
-list.files()
-# helper function for getting the hospital name
-helper <- function(data, col_num, state) {
-    state_subset <- data[data[, 7]==state, ]
-    outcome_arr <- state_subset[, col_num]
-    min <- min(outcome_arr, na.rm=T)
-    min_index <- which(outcome_arr == min)
-    hosp_name <- state_subset[min_index, 2]
-    return(hosp_name)
-}
-
-best <- function(state, outcome) {
-    ## Read outcome data
-    ## Check that state and outcome are valid
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
-    
-    # read the data file
-    directory <- "./data/outcome-of-care-measures.csv"
-    data <- read.csv(directory, colClasses="character")
-    # change data type from character to numeric
-    data[, 11] <- as.numeric(data[, 11]) # heart attack
-    data[, 17] <- as.numeric(data[, 17]) # heart failure
-    data[, 23] <- as.numeric(data[, 23]) # pneumonia
-    valid_outcomes <- c("heart attack", "heart failure", "pneumonia")
-    if (!state %in% data$State) {
-        stop("invalid state")
-    } else if(!outcome %in% valid_outcomes) {
-        stop("invalid outcome")
+##  Function to look through at CSV table for the highest rated hosipital based on 3 categories
+best=function(state, outcome) {
+  state=toupper(state)
+  outcome=tolower(outcome)
+  FileName="outcome-of-care-measures.csv"
+  data=read.csv(FileName, colClasses="character")
+  
+  CorrectInput=c("heart attack", "heart failure", "pneumonia")
+  if (!state %in% data$State) {
+    stop("Invalid State")
+  } else if(!outcome %in% CorrectInput) {
+    stop("Invalid Cause")
+  } else {
+    if(outcome == "heart attack") {
+      data[, 11]=as.numeric(data[, 11])
+      HospitalName=finder(data, 11, state)
+    } else if(outcome == "heart failure") {
+      data[, 17]=as.numeric(data[, 17])
+      HospitalName=finder(data, 17, state)
     } else {
-        if(outcome == "heart attack") {
-            hosp_name <- helper(data, 11, state)
-        } else if(outcome == "heart failure") {
-            hosp_name <- helper(data, 17, state)
-        } else {
-            hosp_name <- helper(data, 23, state)
-        }
-        result <- hosp_name
-        return(result)
+      data[, 23]=as.numeric(data[, 23])
+      HospitalName=finder(data, 23, state)
     }
-}
+    result=HospitalName
+    return(result)
+  }
 
-# tests
-#best("TX", "heart attack")
-#best("TX", "heart failure")
-#best("MD", "heart attack")
-#best("MD", "pneumonia")
-#best("BB", "heart attack")
+}
+##  Function to use inside <best> instead of running the sorter 3 times in the If/Else loops
+finder=function(data, col_num, state) {
+  StateSub=data[data[, 7]==state, ]
+  StateValue=StateSub[, col_num]
+  min=min(StateValue, na.rm=T)
+  min_index=which(StateValue == min)
+  View(min_index)
+  HospitalName=StateSub[min_index, 2]
+  return(HospitalName)
+}
