1855e78fdb98f7fbba1ad949364369e4aaddacc2
Matt Beam <wmbeam02@users.noreply.github.com>
2015-05-30 21:20:20 -0500

    Create OrderDataFramePt2.R

---
+++ OrderDataFramePt2.R
@@ -0,0 +1,31 @@
+##  A function to sort a dataframe first by State then to order the data in lowest occurrence to highest with the names of the 
+##  hospitals alphabetic relationship used to determine ties.
+rankhospital=function(state, outcome, num="best") {
+  FileName="outcome-of-care-measures.csv"
+  data=read.csv(FileName, colClasses="character", na.strings="Not Available")
+  ## Check that state and outcome are valid
+  validOutcome = c("heart attack","heart failure","pneumonia")
+  if (!outcome %in% validOutcome) { stop("invalid outcome")}
+  
+  validState = unique(data[,7])
+  if (!state %in% validState) stop("invalid state")
+  
+  ## convert outcome name into column name
+  fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+  colName <- fullColName[match(outcome,validOutcome)]
+  
+  ## Return hospital name in that state with the given rank 30-day death rate
+  data.state <- data[data$State==state,]
+  
+  # order data by outcome
+  sorted.data.state <- data.state[order(as.numeric(data.state[[colName]]),data.state[["Hospital.Name"]],decreasing=FALSE,na.last=NA), ]
+  
+  #handle num input
+  if (num=="best") num = 1
+  if (num=='worst') num = nrow(sorted.data.state)
+  #will automatically return NA if num > nrow, as well as if it's some other text value
+  # if someone passes num < 1, they'll get what's expected
+  #if (is.numeric(num) & num > nrwo(sorted.data.state) return(NA)
+  
+  sorted.data.state[num,"Hospital.Name"]
+}
